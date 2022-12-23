0056af47fa66176a4515f6e9afb3dbbacef066e0
angie-at-work <121167390+angie-at-work@users.noreply.github.com>
2022-12-23 12:35:13 +0100

    HashRouter part 6

--- src/Header.js
+++ src/Header.js
@@ -1,27 +1,29 @@
 import logo from './images/logo.svg';
-import { Link } from "react-router-dom";
+import { HashRouter, Link } from "react-router-dom";
 //import './styles/Header.css';
 
 function Header() {
 	return (
-		<header className="App-header">
-			<div className="wrapper">
-				<a href="/"><img src={logo} className="logo" alt="logo" /></a>
-				<nav>
-					<ul id="menu">
-						<li>
-							<Link to="/about">About us</Link>
-						</li>
-						<li>
-							<Link to="/gallery">Gallery</Link>
-						</li>
-						<li>
-							<Link to="/contacts">Contacts</Link>
-						</li>
-					</ul>
-				</nav>
-			</div>
-		</header>
+		<HashRouter basename="/">
+			<header className="App-header">
+				<div className="wrapper">
+					<a href="/"><img src={logo} className="logo" alt="logo" /></a>
+					<nav>
+						<ul id="menu">
+							<li>
+								<Link to="/about">About us</Link>
+							</li>
+							<li>
+								<Link to="/gallery">Gallery</Link>
+							</li>
+							<li>
+								<Link to="/contacts">Contacts</Link>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</header>
+		</HashRouter>
 	);
 }
 
