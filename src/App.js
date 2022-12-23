90778b6f54cba8ef0daef23e3fd883c7861badd0
angie-at-work <121167390+angie-at-work@users.noreply.github.com>
2022-12-23 12:38:47 +0100

    changes to App.js

--- src/App.js
+++ src/App.js
@@ -13,32 +13,14 @@ function App() {
 	return (		
 			<div className="page-wrapper">
 				<HelmetProvider>
+					<Header />
 					<HashRouter basename="/">
-						{/* <Header /> */}
-						<header className="App-header">
-							<div className="wrapper">
-								<a href="/"><img src={logo} className="logo" alt="logo" /></a>
-								<nav>
-									<ul id="menu">
-										<li>
-											<Link to="/about">About us</Link>
-										</li>
-										<li>
-											<Link to="/gallery">Gallery</Link>
-										</li>
-										<li>
-											<Link to="/contacts">Contacts</Link>
-										</li>
-									</ul>
-								</nav>
-							</div>
-						</header>
 						<Route exact path="/" component={Home} />
 						<Route exact path="/about" component={About} />
 						<Route exact path="/gallery" component={Gallery} />
 						<Route exact path="/contacts" component={Contacts} />
-						<Footer />
 					</HashRouter>
+					<Footer />
 				</HelmetProvider>
 			</div>
 	);
