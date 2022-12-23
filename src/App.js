4a4e1e46aa7f5af5f88dda110d4cd91a282eeb83
angie-at-work <121167390+angie-at-work@users.noreply.github.com>
2022-12-23 12:23:38 +0100

    hashrouter part 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { HelmetProvider } from 'react-helmet-async';
 import Header from './Header';
+import logo from './images/logo.svg';
 import Home from './Home';
 import About from './About';
 import Gallery from './Gallery';
 import Contacts from './Contacts';
 import Footer from './Footer';
-import { HashRouter, Route } from "react-router-dom";
+import { HashRouter, Route, Link } from "react-router-dom";
 import './styles/index.css';
 
 function App() {
@@ -13,7 +14,25 @@ function App() {
 			<div className="page-wrapper">
 				<HelmetProvider>
 					<HashRouter basename="/">
-						<Header />
+						{/* <Header /> */}
+						<header className="App-header">
+							<div className="wrapper">
+								<a href="/"><img src={logo} className="logo" alt="logo" /></a>
+								<nav>
+									<ul id="menu">
+										<li>
+											<Link to="/about">About us</Link>
+										</li>
+										<li>
+											<Link to="/gallery">Gallery</Link>
+										</li>
+										<li>
+											<Link to="/contacts">Contacts</Link>
+										</li>
+									</ul>
+								</nav>
+							</div>
+						</header>
 						<Route exact path="/" component={Home} />
 						<Route exact path="/about" component={About} />
 						<Route exact path="/gallery" component={Gallery} />
